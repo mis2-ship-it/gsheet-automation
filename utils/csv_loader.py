6b35d2c6b73c6fc4dd32d01278c7104d4f1bb20f
MIS_FB <mis2@frozenbottle.in>
2026-07-28 20:01:42 +0530

    Update csv_loader.py

--- utils/csv_loader.py
+++ utils/csv_loader.py
@@ -1,1 +1,38 @@
+from pathlib import Path
+from datetime import datetime
+import pandas as pd
 
+
+def load_current_month_data():
+    """
+    Automatically loads the current month's MTD CSV.
+
+    Example:
+    monthly_data/2026/MTD_Jul_26.csv
+    """
+
+    today = datetime.now()
+
+    year_folder = str(today.year)
+
+    file_name = today.strftime("MTD_%b_%y.csv")
+
+    csv_path = (
+        Path("monthly_data")
+        / year_folder
+        / file_name
+    )
+
+    if not csv_path.exists():
+        raise FileNotFoundError(
+            f"Monthly file not found:\n{csv_path}"
+        )
+
+    print(f"📂 Loading : {csv_path}")
+
+    df = pd.read_csv(
+        csv_path,
+        low_memory=False
+    )
+
+    return df
